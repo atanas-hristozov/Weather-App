4570a82458b4c22b2b182bd96eade6b63bb3be6a
atanas-hristozov <64282150+atanas-hristozov@users.noreply.github.com>
2021-10-31 18:36:04 +0200

    Add files via upload

---
+++ WeatherApp/config.js
@@ -0,0 +1,7 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "cd69d896fcd54109fef499694bc2e116"
+    }
+}
+module.exports = constants;
